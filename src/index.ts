b3ba8e38ec2f5022a961c72dc3b346ca212d4947
L2NE-dev <conway24b@gmail.com>
2026-04-27 22:04:39 +0700

    Removed fonts...

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,6 @@
 export * from "./scss/runtime/index";
 export { loadVeelaVariant } from "./scss/runtime/index";
 
-// Font utilities
-export * from "./ts/font-loader";
-
 // Default export for backwards compatibility
 import forDefault from "./scss/runtime/index";
 export default forDefault;
--- src/ts/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Veela.CSS TypeScript Module
- *
- * Exports font loading utilities and type definitions.
- * Runtime styles and initialization are in ../scss/runtime/index.ts
- */
-
-export * from './font-loader';
-export type { FontMetadata } from './font-loader';
